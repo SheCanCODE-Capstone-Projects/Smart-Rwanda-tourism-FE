f47f1a80a48f9bab1eba6bd21ffde4fca492d5ec
paccy3736 <pascalineikuzwe@gmail.com>
2026-07-22 11:30:09 +0200

    feat: add LoginPage and RegisterPage exports to pages barrel and update App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { ForgotPassword, ResetPassword } from './pages';
+import { ForgotPassword, ResetPassword, LoginPage, RegisterPage } from './pages';
 
 function App() {
   return (
     <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
-        <Route path="/" element={<Navigate to="/forgot-password" replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 export { default as ForgotPassword } from './ForgotPassword';
 export { default as ResetPassword } from './ResetPassword';
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
